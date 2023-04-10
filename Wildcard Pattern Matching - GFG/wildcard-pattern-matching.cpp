fcb8bb65cbe919d0162e6e36c7e8002fe15b81dd
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-04-10 14:48:24 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -26,17 +26,8 @@ class Solution{
         
         for(int i=1; i<=np; i++){
             for(int j=1; j<=ns; j++){
-                if(p[i-1]==s[j-1]) dp[i][j]=dp[i-1][j-1];
-                else if(p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
-                else if (p[i-1]=='*'){
-                    for(int k=j; k>=0; k--){
-                        if(dp[i-1][k]){
-                            dp[i][j]=1;
-                            break;
-                        }
-                    }
-                }
-                else dp[i][j]=0;
+                if(p[i-1]==s[j-1]||p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
+                else if (p[i-1]=='*') dp[i][j]=dp[i-1][j]||dp[i][j-1];
             }
         }
         
