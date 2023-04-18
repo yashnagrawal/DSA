4259e283db86825abbd9a7aa5622bd7cdf5f922b
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-04-18 17:03:28 +0530

    Added solution - LeetHub

--- Print all LCS sequences - GFG/print-all-lcs-sequences.cpp
+++ Print all LCS sequences - GFG/print-all-lcs-sequences.cpp
@@ -7,56 +7,55 @@ class Solution
 {
 	public:
 	    vector<string> ans;
-	    
-	    void lexicographical_lcs_helper(string lcs, int si, int ti, int ns, int nt, string s, string t, vector<vector<int>> &dp){
-	        if(lcs.length()==dp[0][0]){
-	            ans.push_back(lcs);
+	    void lcs_adder(string str, int si, int sj, int ns, int nt, string s, string t, vector<vector<int>> &dp){
+	        if(str.length()==dp[0][0]){
+	            ans.push_back(str);
 	            return;
 	        }
 	        
-	        if(si==ns||ti==nt) return;
+	       // if(si>=ns||sj>=nt) return;
 	        
 	        for(char ch = 'a'; ch<='z'; ch++){
 	            bool is_ch_included = 0;
-	            for(int i=si; i<ns; i++){
-    	            if(ch!=s[i]) continue;
-    	            for(int j=ti; j<nt; j++){
-    	                if(ch==t[j]&&(dp[i][j]+lcs.length()==dp[0][0])){
-    	                    string new_lcs = lcs+s[i];
-    	                   // cout<<"\n starting ("<<i<<", "<<j<<") "<<lcs<<s[i]<<" ";
-    	                    lexicographical_lcs_helper(new_lcs, i+1, j+1, ns, nt, s, t, dp);
-    	                   // cout<<"\n ended ("<<i<<", "<<j<<") "<<lcs<<s[i]<<" ";
-    	                    is_ch_included = 1;
-    	                    break;
-    	                }
-    	            }
-    	            if(is_ch_included) break;
-    	        }
-	        }
+		        for(int i=si; i<ns; i++){
+		            if(ch!=s[i]) continue;
+		            for(int j=sj; j<nt; j++){
+		                if(ch!=t[j]||dp[i][j]+str.length()!=dp[0][0]) continue;
+		                
+		                string new_str = str + s[i];
+		                lcs_adder(new_str, i+1, j+1, ns, nt, s, t, dp);
+		                is_ch_included = 1;
+		                break;
+		            }
+		            if(is_ch_included) break;
+		        }
+		    }
 	    }
-	    
+	
 		vector<string> all_longest_common_subsequences(string s, string t)
 		{
 		    // Code here
+		    
 		    int ns = s.length(), nt = t.length();
 		    
-		    vector<vector<int>> dp_reverse(ns+1, vector<int> (nt+1, 0));
+		    vector<vector<int>> lcs_length(ns+1, vector<int> (nt+1, 0));
 		    
+		    //lcs_length[i][j] s[i:ns-1] t[j:nt-1]
 		    
 		    for(int i=ns-1; i>=0; i--){
 		        for(int j=nt-1; j>=0; j--){
-		            if(s[i]==t[j]) dp_reverse[i][j]=1+dp_reverse[i+1][j+1];
-		            else dp_reverse[i][j]=max(dp_reverse[i+1][j], dp_reverse[i][j+1]);
+		            if(s[i]==t[j]) lcs_length[i][j]=1+lcs_length[i+1][j+1];
+		            else lcs_length[i][j]=max(lcs_length[i+1][j], lcs_length[i][j+1]);
 		        }
 		    }
 		    
+		    lcs_adder("", 0, 0, ns, nt, s, t, lcs_length);
 		    
-		    lexicographical_lcs_helper("", 0, 0, ns, nt, s, t, dp_reverse);
 		    
 		    return ans;
+		    
 		}
 };
-
 
 
 
