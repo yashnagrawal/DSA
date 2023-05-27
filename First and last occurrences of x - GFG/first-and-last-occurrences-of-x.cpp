9a3f28b319012087a72fe6e0a26f54266db1f37b
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-05-27 11:21:40 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -3,57 +3,16 @@
 using namespace std;
 
 // } Driver Code Ends
-
-bool is_num_greater_than_equal_to_x(int num, int x){
-    return num>=x;
-}
-
-bool is_num_greater_than_x(int num, int x){
-    return num>x;
-}
-
 vector<int> find(int arr[], int n , int x )
 {
     // code here
-    vector<int> ans;
+    auto lo = lower_bound(arr, arr+n, x);
     
-    if(arr[0]>x||arr[n-1]<x) return {-1, -1};
+    if(lo==arr+n||*lo!=x) return {-1, -1};
     
-    int lo = 0;
-    int hi = n;
+    auto up = upper_bound(arr, arr+n, x);
     
-    while(lo<hi){
-        int mid = lo + (hi-lo)/2;
-        
-        if(is_num_greater_than_equal_to_x(arr[mid], x)){
-            hi = mid;
-        }
-        else{
-            lo = mid + 1;
-        }
-    }
-    
-    if(arr[lo]!=x) return {-1, -1};
-    
-    ans.push_back(lo);
-    
-    lo = 0;
-    hi = n;
-    
-    while(lo<hi){
-        int mid = lo + (hi-lo)/2;
-        
-        if(is_num_greater_than_x(arr[mid], x)){
-            hi = mid;
-        }
-        else{
-            lo = mid + 1;
-        }
-    }
-    
-    ans.push_back(lo-1);
-    
-    return ans;
+    return {lo-arr, up-arr-1};
 }
 
 //{ Driver Code Starts.
