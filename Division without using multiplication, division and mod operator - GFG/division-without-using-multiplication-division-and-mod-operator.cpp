aadb3cd3dcc9c70bc34c49104b512e27c5861c1e
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-05-30 15:42:41 +0530

    Added solution - LeetHub

---
+++ Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    long long divide(long long dividend, long long divisor) 
+    {
+        //code here
+        
+        int neg = ((dividend>0)^(divisor>0))?(-1):(1);
+        
+        dividend=abs(dividend);
+        divisor=abs(divisor);
+        
+        long long quotient = 0;
+        
+        int temp = 0;
+        
+        for(int i=31; i>=0; i--){
+            if((temp+(divisor<<i))<=dividend){
+                temp +=(divisor<<i);
+                quotient|=(long long)1<<i;
+            }
+        }
+        
+        return neg*quotient;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+
+		long long a, b;
+		cin >> a >> b;
+		
+		Solution ob;
+		cout << ob.divide(a, b) << "\n";
+	}
+
+	return 0;
+}
+
+// } Driver Code Ends
