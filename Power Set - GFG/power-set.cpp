02378dc33480d5bbaaa1cf0460e9dee500a18f94
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-05-31 18:07:12 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.cpp
+++ Power Set - GFG/power-set.cpp
@@ -5,23 +5,17 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
-	    vector<string> makeSubstringsWithIndex(string s, int ind){
-	        if(ind<0) return {""};
-	        
-	        vector<string> ans = makeSubstringsWithIndex(s, ind-1);
-	        
-	        int n = ans.size();
-	        for(int i=0; i<n; i++){
-	            string str = ans[i] + s[ind];
-	            ans.push_back(str);
-	        }
-	        
-	        return ans;
-	    }
 		vector<string> AllPossibleStrings(string s){
 		    // Code here
     		int n = (int)s.length();
-		    vector<string> ans = makeSubstringsWithIndex(s, n-1);
+		    vector<string> ans(1<<n);
+		    
+		    for(int i=0; i<(1<<n); i++){
+		        for(int j=0; j<n; j++){
+		            if(i&(1<<j)) ans[i].push_back(s[j]);
+		        }
+		    }
+		    
 		    sort(ans.begin(), ans.end());
 		    ans.erase(ans.begin());
 		    return ans;
