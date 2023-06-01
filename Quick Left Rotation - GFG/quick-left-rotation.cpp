c093be38bc1e44b21c922bed5020c1b8d1aecbcf
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-01 23:07:16 +0530

    Added solution - LeetHub

---
+++ Quick Left Rotation - GFG/quick-left-rotation.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	void leftRotate(int arr[], int k, int n) 
+	{ 
+	   // Your code goes here
+	   vector<int> dup(n);
+	   for(int i=0; i<n; i++){
+	       dup[i]=arr[i];
+	   }
+	   
+	   for(int i=0; i<n; i++){
+	       arr[i]=dup[(i+k)%n];
+	   }
+	} 
+		 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+	    cin >> n;
+	    int k;
+	    cin >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	        // um[a[i]]++;
+	    }
+
+
+       
+
+        Solution ob;
+        ob.leftRotate(a,k,n);
+        
+        for (int i = 0; i < n; i++) 
+        	cout << a[i] << " "; 
+
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
