a3dbefa64e220b099d580c3f8b884d49d9b3d1cd
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-01 23:45:53 +0530

    Added solution - LeetHub

--- Quick Left Rotation - GFG/quick-left-rotation.cpp
+++ Quick Left Rotation - GFG/quick-left-rotation.cpp
@@ -7,17 +7,30 @@ class Solution{
 	
 	
 	public:
+	void reverseArray(int arr[], int st, int ed){
+	    for(int i=st; i<=(st+(ed-st)/2); i++){
+	        int temp = arr[i];
+	        arr[i]=arr[ed-(i-st)];
+	        arr[ed-(i-st)]=temp;
+	    }
+	}
+	
 	void leftRotate(int arr[], int k, int n) 
 	{ 
 	   // Your code goes here
-	   vector<int> dup(n);
-	   for(int i=0; i<n; i++){
-	       dup[i]=arr[i];
-	   }
+	   k%=n;
 	   
-	   for(int i=0; i<n; i++){
-	       arr[i]=dup[(i+k)%n];
-	   }
+	   if(k==0) return;
+	   
+	   //reverse first d elements
+	   reverseArray(arr, 0, k-1);
+
+	   //reverse the left n-d elements
+	   reverseArray(arr, k, n-1);
+
+	   //reverse the whole array
+	   reverseArray(arr, 0, n-1);
+	   
 	} 
 		 
 
