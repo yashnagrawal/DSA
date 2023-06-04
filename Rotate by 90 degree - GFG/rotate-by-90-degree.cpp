f1de23e6f8dc7ce826d0116d26b7eaed8196bdd7
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-04 21:38:52 +0530

    Added solution - LeetHub

--- Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
@@ -9,14 +9,16 @@ class Solution
     public:
     //Function to rotate matrix anticlockwise by 90 degrees.
     void rotateby90(vector<vector<int> >& mat, int n) 
-    { 
-        vector<vector<int>> org_mat(mat);
-        
+    {
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                mat[n-j-1][i]=org_mat[i][j];
+            for(int j=i+1; j<n; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
+        
+        for(int i=0; i<n/2; i++) mat[i].swap(mat[n-i-1]);
     } 
 };
 
