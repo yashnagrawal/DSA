b6034c67172a67559a9ed1a0cc48df2492d8da4b
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-05 15:12:18 +0530

    Added solution - LeetHub

--- Max sum in sub-arrays - GFG/max-sum-in-subarrays.cpp
+++ Max sum in sub-arrays - GFG/max-sum-in-subarrays.cpp
@@ -14,25 +14,25 @@ class Solution{
         long long ans = INT_MIN;
         stack<long long> s1, s2;
         
-        vector<long long> next_smallest_ind(n, INT_MIN), prev_smallest_ind(n, INT_MIN);
+        vector<long long> next_smallest(n, INT_MIN), prev_smallest(n, INT_MIN);
         
         for(int i=n-1; i>=0; i--){
             while((!s1.empty())&&(arr[i]<s1.top())) s1.pop();
             
-            if(!s1.empty()) next_smallest_ind[i]=s1.top();
+            if(!s1.empty()) next_smallest[i]=s1.top();
             s1.push(arr[i]);
         }
         
         for(int i=0; i<n; i++){
             while((!s2.empty())&&(arr[i]<s2.top())) s2.pop();
             
-            if(!s2.empty()) prev_smallest_ind[i]=s2.top();
+            if(!s2.empty()) prev_smallest[i]=s2.top();
             s2.push(arr[i]);
         }
         
         
         for(int i=0; i<n; i++){
-            long long min1 = min1 = max(prev_smallest_ind[i], next_smallest_ind[i]);
+            long long min1 = max(prev_smallest[i], next_smallest[i]);
             long long min2 = arr[i];
             
             ans=max(ans, min1+min2);
