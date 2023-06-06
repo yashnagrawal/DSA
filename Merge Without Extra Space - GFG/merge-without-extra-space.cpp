845ec252318c05b203faf4b08f80392be9f79d51
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-06 19:16:47 +0530

    Added solution - LeetHub

---
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here 
+            vector<long long> sorted(n+m);
+            
+            for(int i=0; i<n; i++) sorted[i] = arr1[i];
+            
+            for(int i=0; i<m; i++) sorted[i+n] = arr2[i];
+            
+            sort(sorted.begin(), sorted.end());
+            
+            for(int i=0; i<n; i++) arr1[i]=sorted[i];
+            
+            for(int i=n; i<n+m; i++) arr2[i-n]=sorted[i];
+        } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
