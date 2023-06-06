8bcc7ed8a0bc26c5c493b3d2b20446888db59a79
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-06 19:13:16 +0530

    Added solution - LeetHub

---
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        // code here
+        unordered_map<int, int> freq;
+        
+        int a, b;
+        
+        for(auto i: arr) freq[i]++;
+        
+        for(int i=1; i<=n; i++){
+            if(freq[i]==0) a = i;
+            if(freq[i]==2) b = i;
+        }
+        
+        return {b, a};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
