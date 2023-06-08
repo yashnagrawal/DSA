d851c9689c3ea6748eaf45b40985d4125a198c91
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-08 14:49:52 +0530

    Added solution - LeetHub

--- Pascal Triangle - GFG/pascal-triangle.cpp
+++ Pascal Triangle - GFG/pascal-triangle.cpp
@@ -5,16 +5,41 @@ using namespace std;
 #define ll long long
 
 // } Driver Code Ends
-// #define ll long long
+#define ll long long
 class Solution{
 public:
-    vector<ll> nthRowOfPascalTriangle(int n) {
+    vector<ll> dp;
+    ll mod = 1000000007;
+    ll moudular_pow(ll n, ll pow){
+        if(pow==0) return 1;
+        
+        ll ans = moudular_pow(n, pow/2);
+        ans=(ans*ans)%mod;
+        if(pow%2) ans=(ans*n)%mod;
+        
+        return ans;
+    }
+    ll modular_inv(ll n){
+        moudular_pow(n, mod-2);
+    }
+    
+    ll c(ll n, ll r){
+        ll num = dp[n];
+        
+        ll den = (dp[r]*dp[n-r])%mod;
+        
+        return (num*modular_inv(den))%mod;
+    }
+    
+    vector<ll> nthRowOfPascalTriangle(ll n) {
         // code here
+        dp.push_back(1);
+        for(int i=1; i<n; i++) dp.push_back((dp[i-1]*i)%mod);
+        
         vector<ll> ans(n, 1ll);
-        for(int i=2; i<=n; i++){
-            for(int j=i-2; j>0; j--){
-                ans[j]=(ans[j]+ans[j-1])%1000000007;
-            }
+        
+        for(int j=0; j<n; j++){
+            ans[j]=c(n-1, j);
         }
         
         return ans;
