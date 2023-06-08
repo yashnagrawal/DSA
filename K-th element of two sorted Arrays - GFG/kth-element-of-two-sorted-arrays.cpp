233ff47b8c5b7a23161ffc5226d74d460d5c564d
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-08 18:12:47 +0530

    Added solution - LeetHub

---
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        vector<int> sorted;
+        
+        int i=0;
+        int j=0;
+        
+        int counter = 0;
+        int curr_element = INT_MIN;
+        
+        while((i<n||j<m)&&(i+j<k)){
+            if(j==m||(i<n&&arr1[i]<arr2[j])) curr_element=arr1[i++];
+            else curr_element=arr2[j++];
+        }
+        
+        return curr_element;
+    }
+};
+
+//{ Driver Code Starts.
+ 
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m,k;
+		cin>>n>>m>>k;
+		int arr1[n],arr2[m];
+		for(int i=0;i<n;i++)
+			cin>>arr1[i];
+		for(int i=0;i<m;i++)
+			cin>>arr2[i];
+		
+		Solution ob;
+        cout << ob.kthElement(arr1, arr2, n, m, k)<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
