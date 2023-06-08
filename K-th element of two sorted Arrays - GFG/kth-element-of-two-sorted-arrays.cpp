204c61edca222386f901d0e457a53ba4da607404
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-08 21:15:36 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -6,19 +6,39 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int kthElement(int arr1[], int arr2[], int n, int m, int k)
-    {
-        int i=0;
-        int j=0;
+    
+    int num_of_elements_greater_than_x(int x, int arr[], int n){
+        int lo = 0;
+        int hi = n;
         
-        int curr_element = INT_MIN;
-        
-        while((i<n||j<m)&&(i+j<k)){
-            if(j==m||(i<n&&arr1[i]<arr2[j])) curr_element=arr1[i++];
-            else curr_element=arr2[j++];
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+            
+            if(arr[mid]>x) hi = mid;
+            else lo = mid + 1;
         }
         
-        return curr_element;
+        return n-lo;
+    }
+    
+    bool f(int x, int z, int arr1[], int arr2[], int n, int m){
+        // are_there_less_than_z_elements_less_than_x_in_merged_arr
+        return num_of_elements_greater_than_x(x, arr1, n)+num_of_elements_greater_than_x(x, arr2, m)<=z;
+    }
+    
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        int lo = min(arr1[0], arr2[0]);
+        int hi = max(arr1[n-1], arr2[m-1]);
+        
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+            
+            if(f(mid, n+m-k, arr1, arr2, n, m)) hi = mid;
+            else lo = mid + 1;
+        }
+        
+        return lo;
     }
 };
 
