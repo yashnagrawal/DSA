eb4760e4c4684a58590db94f462c726ce31431fe
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-10 12:11:35 +0530

    Added solution - LeetHub

---
+++ K-th missing element - GFG/kth-missing-element.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int KthMissingElement(int a[], int n, int k);
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        
+        cout << KthMissingElement(a, n, k) << endl;
+    
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+int KthMissingElement(int a[], int n, int k)
+{
+    // Complete the function
+    if(a[n-1]-a[0]-n+1<k) return -1;
+    
+    int lo = 0;
+    int hi = n-1;
+    
+    while(lo<hi){
+        int mid = lo + (hi-lo)/2;
+        
+        int num_of_missing_elements = (a[mid]-a[0]+1)-(mid-0+1);
+        
+        if(num_of_missing_elements>=k) hi = mid;
+        else lo = mid + 1;
+    }
+    
+    lo--;
+    
+    int num_of_missing_elements = (a[lo]-a[0]+1)-(lo-0+1);
+    
+    return a[lo]+(k-num_of_missing_elements);
+}
