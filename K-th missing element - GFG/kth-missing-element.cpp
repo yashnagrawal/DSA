e1fdfbe61c04ebf836ef08eb564389af0a52ed33
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-10 14:21:51 +0530

    Added solution - LeetHub

--- K-th missing element - GFG/kth-missing-element.cpp
+++ K-th missing element - GFG/kth-missing-element.cpp
@@ -31,16 +31,32 @@ int main()
 int KthMissingElement(int a[], int n, int k)
 {
     // Complete the function
+    // index -> 0 1 2 3 4 5
+    // arr   -> 1 3 4 5 7 9
+    // diff  -> 0 1 1 1 2 3
+    // bb    -> 0 0 0 0 1 1
+    // diff = (a[i]-a[0]+1)-(i+1) number of missing elements till index i
+    // if diff>=k bb = 1 else bb = 0
     
-    int num_of_missing_elements = 0;
-    unordered_set<int> nums_present;
+    int total_missing_elements = a[n-1]-a[0]-(n);
     
-    for(int i=0; i<n; i++) nums_present.insert(a[i]);
+    if(total_missing_elements<k) return -1;
     
-    for(int num=a[0]; num<=a[n-1]; num++){
-        if(nums_present.find(num)==nums_present.end()) num_of_missing_elements++;
-        if(num_of_missing_elements==k) return num;
+    int lo = 0;
+    int hi = n-1;
+    
+    while(lo<hi){
+        int mid = lo + (hi-lo)/2;
+        
+        int num_of_missing_elements = (a[mid]-a[0]+1)-(mid+1);
+        
+        if(num_of_missing_elements>=k) hi = mid;
+        else lo = mid + 1;
     }
     
-    return -1;
+    lo--;
+    
+    int missing_element = a[lo]+k-((a[lo]-a[0]+1)-(lo+1));
+    
+    return missing_element;
 }
