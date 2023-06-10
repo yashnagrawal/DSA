66c328633ea5e894be964f0ba2aaa186ab0022ad
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-10 14:00:10 +0530

    Added solution - LeetHub

--- K-th missing element - GFG/kth-missing-element.cpp
+++ K-th missing element - GFG/kth-missing-element.cpp
@@ -32,25 +32,16 @@ int KthMissingElement(int a[], int n, int k)
 {
     // Complete the function
     
-    for(int i=1; i<n; i++){
-        int elements_required = a[i]-a[0]+1;
-        int elements_present = i-0+1;
-        
-        int missing_elements = elements_required - elements_present;
-        
-        if(missing_elements>=k){
-            unordered_set<int> s;
-            
-            for(int j=0; j<i; j++) s.insert(a[j]);
-            
-            int curr_missing_elements_count = 0;
-            
-            for(int l=a[0]; l<a[i]; l++){
-                if(s.find(l)==s.end()) curr_missing_elements_count++;
-                if(curr_missing_elements_count==k) return l;
-            }
-        }
+    vector<int> missing_elements;
+    unordered_set<int> s;
+    
+    for(int i=0; i<n; i++) s.insert(a[i]);
+    
+    for(int num=a[0]; num<=a[n-1]; num++){
+        if(s.find(num)==s.end()) missing_elements.push_back(num);
     }
     
-    return -1;
+    if(missing_elements.size()<k) return -1;
+    
+    return missing_elements[k-1];
 }
