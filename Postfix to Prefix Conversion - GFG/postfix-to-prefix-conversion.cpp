c382062dfacb5f95ea6077d917491a7d6248a563
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-06-15 13:05:48 +0530

    Added solution - LeetHub

--- Postfix to Prefix Conversion - GFG/postfix-to-prefix-conversion.cpp
+++ Postfix to Prefix Conversion - GFG/postfix-to-prefix-conversion.cpp
@@ -12,29 +12,25 @@ class Solution {
     bool isOperator(char ch){
         return ch=='+'||ch=='-'||ch=='*'||ch=='/';
     }
-    
-    string postToPreHelper(string &post_exp, int &ind){
-        string prefix = "";
-        if(ind<0) return prefix;
-        
-        if(!isOperator(post_exp[ind])){
-            prefix = post_exp[ind--];
-            return prefix;
-        }
-        
-        char op = post_exp[ind--];
-        string right = postToPreHelper(post_exp, ind);
-        string left = postToPreHelper(post_exp, ind);
-        
-        prefix = op + left + right;
-        
-        return prefix;
-    }
     string postToPre(string post_exp) {
         // Write your code here
         int n = post_exp.length();
-        int ind = n-1;
-        return postToPreHelper(post_exp, ind);
+        stack<string> stk;
+        
+        for(auto ch: post_exp){
+            string ch_str = string(1, ch);
+            
+            if(!isOperator(ch)) stk.push(ch_str);
+            else{
+                string right = stk.top();
+                stk.pop();
+                string left = stk.top();
+                stk.pop();
+                stk.push(ch_str + left + right);
+            }
+        }
+        
+        return stk.top();
     }
 };
 
