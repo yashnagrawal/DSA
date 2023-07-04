faf02a79693b3f52dadd146cc58958032cc2cd1f
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-07-04 15:15:01 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,37 +10,36 @@ using namespace std;
 
 class Solution {
   public:
-    bool isNodeConnectedToCyclic(int node, vector<int> adj[], vector<int> &visited, vector<bool> &ancestor){
-        visited[node] = 1;
-        ancestor[node] = 1;
-        
-        for(auto child: adj[node]){
-            if(ancestor[child]) visited[node] = 2;
-            
-            if(visited[child]==0){
-                if(isNodeConnectedToCyclic(child, adj, visited, ancestor)) visited[node] = 2;
-            }
-            else if(visited[child]==2) visited[node] = 2;
-        }
-        
-        ancestor[node] = 0;
-        return visited[node]==2;
-    }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         // code here
-        vector<int> visited(V, 0);
-        vector<bool> ancestor(V, 0);
-        
-        for(int i=0; i<V; i++){
-            if(!visited[i]) isNodeConnectedToCyclic(i, adj, visited, ancestor);
-        }
-        
+        vector<int> al[V];
+        vector<int> indegree(V, 0);
         vector<int> safeNodes;
+        queue<int> q;
         
         for(int i=0; i<V; i++){
-            if(visited[i]==1) safeNodes.push_back(i);
+            for(auto j: adj[i]) al[j].push_back(i);
+            
+            indegree[i] = adj[i].size();
         }
         
+        for(int i=0; i<V; i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int front = q.front();
+            safeNodes.push_back(front);
+            q.pop();
+            
+            for(auto child: al[front]){
+                indegree[child]--;
+                
+                if(indegree[child]==0) q.push(child);
+            }
+        }
+        
+        sort(safeNodes.begin(), safeNodes.end());
         
         return safeNodes;
     }
