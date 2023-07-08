b8282ef6ab0493afe1d65115e90512792f1e713f
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-07-08 21:28:20 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -12,28 +12,31 @@ public:
     int maximumPath(int n, vector<vector<int>> matrix)
     {
         // code here
-        vector<vector<int>> dp(n, vector<int> (n));
+        vector<int> dp(n);
         
         for(int j=0; j<n; j++){
-            dp[0][j] = matrix[0][j];
+            dp[j] = matrix[0][j];
         }
         
         for(int i=1; i<n; i++){
+            vector<int> new_dp(n);
             for(int j=0; j<n; j++){
                 int ans = INT_MIN;
                 
-                if(j>0) ans = max(ans, dp[i-1][j-1]);
-                ans = max(ans, dp[i-1][j]);
-                if(j<n-1) ans = max(ans, dp[i-1][j+1]);
+                if(j>0) ans = max(ans, dp[j-1]);
+                ans = max(ans, dp[j]);
+                if(j<n-1) ans = max(ans, dp[j+1]);
                 
-                dp[i][j] = ans + matrix[i][j];
+                new_dp[j] = ans + matrix[i][j];
             }
+            
+            dp.swap(new_dp);
         }
         
         int max_cost = INT_MIN;
         
         for(int j=0; j<n; j++){
-            max_cost = max(max_cost, dp[n-1][j]);
+            max_cost = max(max_cost, dp[j]);
         }
         
         return max_cost;
