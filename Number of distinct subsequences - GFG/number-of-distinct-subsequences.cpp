4d9941b1e0ecbb81b9b897624cba3f6dc47fbd17
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-07-09 18:30:34 +0530

    Added solution - LeetHub

--- Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
+++ Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
@@ -12,22 +12,32 @@ class Solution{
 	{
 	    // Your code goes here
 	    int n = s.length();
-	    vector<long long> dp(n+1, 1);
-	    //dp[i] a[i-1]
 	    
-	    for(int i=1; i<=n; i++){
-	        dp[i]=(2*dp[i-1])%1000000007;
-	        int k;
-	        for(k=i-1; k>0; k--){
-	            if(s[i-1]==s[k-1]) break;
+	    vector<long long int> dp(n);
+	    
+	    vector<int> last_occ_of_char(26, -1);
+	    
+	    dp[0] = 2;
+	    last_occ_of_char[s[0]-'a'] = 0;
+	    
+	    for(int i=1; i<n; i++){
+	        
+	        if(last_occ_of_char[s[i]-'a']!=-1){
+	            int j = last_occ_of_char[s[i]-'a'];
+	            
+	            if(j==0) dp[i] = (2ll*dp[i-1] - 1)%1000000007;
+	            else dp[i] = (2ll*dp[i-1] - dp[j-1])%1000000007;
+	        }
+	        else{
+	            dp[i] = (2ll*dp[i-1])%1000000007;
 	        }
 	        
-	        if(k>0){
-	            dp[i]=(2*dp[i-1]-dp[k-1]+1000000007)%1000000007;
-	        }
+	        if(dp[i]<0) dp[i]+=1000000007;
+	        
+	        last_occ_of_char[s[i]-'a'] = i;
 	    }
 	    
-	    return dp[n];
+	    return dp[n-1];
 	}
 };
 
