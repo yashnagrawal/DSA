672bd47a48300b14fbbb02590fd16bd1617e00ab
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-07-13 17:00:40 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,27 +9,23 @@ using namespace std;
 
 class Solution{
 public:
-    int matrixMultiplication(int n, int a[])
+    int matrixMultiplication(int n, int arr[])
     {
         // code here
-        vector<vector<int>> dp(n-1, vector<int> (n-1, 0));
+        vector<vector<int>> dp(n-1, vector<int> (n-1, INT_MAX));
+        
+        for(int i=0; i<n-1; i++) dp[i][i] = 0;
         
         for(int len = 1; len<n-1; len++){
             for(int i=0; i+len<n-1; i++){
-                int j = i+len;
-                int ans = INT_MAX;
-                for(int k=i; k<j; k++){
-                    ans=min(ans, dp[i][k]+dp[k+1][j]+((a[i]*a[j+1])*a[k+1]));
-                }
+                int j = i + len;
                 
-                dp[i][j]=ans;
+                for(int k=i; k<j; k++){
+                    int curr = dp[i][k] + dp[k+1][j] + arr[i]*arr[k+1]*arr[j+1];
+                    dp[i][j] = min(dp[i][j], curr);
+                }
             }
         }
-        
-        // for(auto i: dp){
-        //     for(auto j: i) cout<<j<<" ";
-        //     cout<<"\n";
-        // }
         
         return dp[0][n-2];
     }
