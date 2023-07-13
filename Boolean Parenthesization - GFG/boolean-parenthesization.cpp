b879149375ab9d11aa623b3f24f4ea42132bb7f0
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-07-14 00:05:54 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -7,63 +7,48 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-#define MOD 1003
+#define mod 1003
 
 class Solution{
 public:
-    int countWays(int n, string s){
+    int countWays(int n, string str){
         // code here
         vector<vector<int>> dp_true(n, vector<int> (n, 0)), dp_false(n, vector<int> (n, 0));
         
         for(int i=0; i<n; i++){
-            if(s[i]=='T') dp_true[i][i]=1;
-            else if(s[i]=='F') dp_false[i][i]=1;
+            if(str[i]=='T') dp_true[i][i] = 1;
+            else if(str[i]=='F') dp_false[i][i] = 1;
         }
         
-        for(int len = 1; len<n; len++){
-            // cout<<"len: "<<len<<" ";
-            for(int i=0; i+len<n; i++){
-                int j = i+len;
-                // cout<<"i: "<<i<<" j: "<<j<<" ";
+        for(int len=2; len<n; len=len+2){
+            for(int i=0; i+len<n; i=i+2){
+                int j = i + len;
                 
-                for(int k = i+1; k<j; k++){
-                    if(s[k]=='&'){
-                        dp_true[i][j]=(dp_true[i][j]+dp_true[i][k-1]*dp_true[k+1][j])%MOD;
-                        
-                        dp_false[i][j]=(dp_false[i][j]+dp_true[i][k-1]*dp_false[k+1][j])%MOD;
-                        dp_false[i][j]=(dp_false[i][j]+dp_false[i][k-1]*dp_true[k+1][j])%MOD;
-                        dp_false[i][j]=(dp_false[i][j]+dp_false[i][k-1]*dp_false[k+1][j])%MOD;
+                for(int k = i+1; k<j; k=k+2){
+                    char op = str[k];
+                    
+                    if(op=='&'){
+                        dp_true[i][j] = (dp_true[i][j] + dp_true[i][k-1]*dp_true[k+1][j])%mod;
+                        dp_false[i][j] = (dp_false[i][j] + dp_true[i][k-1]*dp_false[k+1][j])%mod;
+                        dp_false[i][j] = (dp_false[i][j] + dp_false[i][k-1]*dp_true[k+1][j])%mod;
+                        dp_false[i][j] = (dp_false[i][j] + dp_false[i][k-1]*dp_false[k+1][j])%mod;
                     }
-                    else if(s[k]=='|'){
-                        dp_true[i][j]=(dp_true[i][j]+dp_true[i][k-1]*dp_true[k+1][j])%MOD;
-                        dp_true[i][j]=(dp_true[i][j]+dp_true[i][k-1]*dp_false[k+1][j])%MOD;
-                        dp_true[i][j]=(dp_true[i][j]+dp_false[i][k-1]*dp_true[k+1][j])%MOD;
-                        
-                        dp_false[i][j]=(dp_false[i][j]+dp_false[i][k-1]*dp_false[k+1][j])%MOD;
+                    else if(op=='|'){
+                        dp_true[i][j] = (dp_true[i][j] + dp_true[i][k-1]*dp_true[k+1][j])%mod;
+                        dp_true[i][j] = (dp_true[i][j] + dp_true[i][k-1]*dp_false[k+1][j])%mod;
+                        dp_true[i][j] = (dp_true[i][j] + dp_false[i][k-1]*dp_true[k+1][j])%mod;
+                        dp_false[i][j] = (dp_false[i][j] + dp_false[i][k-1]*dp_false[k+1][j])%mod;
                     }
-                    else if(s[k]=='^'){
-                        dp_true[i][j]=(dp_true[i][j]+dp_true[i][k-1]*dp_false[k+1][j])%MOD;
-                        dp_true[i][j]=(dp_true[i][j]+dp_false[i][k-1]*dp_true[k+1][j])%MOD;
-                        
-                        dp_false[i][j]=(dp_false[i][j]+dp_true[i][k-1]*dp_true[k+1][j])%MOD;
-                        dp_false[i][j]=(dp_false[i][j]+dp_false[i][k-1]*dp_false[k+1][j])%MOD;
+                    else if(op=='^'){
+                        dp_false[i][j] = (dp_false[i][j] + dp_true[i][k-1]*dp_true[k+1][j])%mod;
+                        dp_true[i][j] = (dp_true[i][j] + dp_true[i][k-1]*dp_false[k+1][j])%mod;
+                        dp_true[i][j] = (dp_true[i][j] + dp_false[i][k-1]*dp_true[k+1][j])%mod;
+                        dp_false[i][j] = (dp_false[i][j] + dp_false[i][k-1]*dp_false[k+1][j])%mod;
                     }
+                    else cout<<"error: "<<i<<", "<<j<<": "<<k<<"\n";
                 }
             }
-            // cout<<"\n";
         }
-        
-        // for(auto i: dp_true){
-        //     for(auto j: i) cout<<j<<" ";
-        //     cout<<"\n";
-        // }
-        
-        // cout<<"\n";
-        
-        // for(auto i: dp_false){
-        //     for(auto j: i) cout<<j<<" ";
-        //     cout<<"\n";
-        // }
         
         return dp_true[0][n-1];
     }
