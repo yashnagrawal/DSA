83a3ef6edc88dde55d0cf7a88c61fc960ebebd65
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-07-18 20:43:46 +0530

    Added solution - LeetHub

---
+++ Shortest Job first - GFG/shortest-job-first.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Back-end complete function Template for C++
+
+class Solution {
+  public:
+    long long solve(vector<int>& bt) {
+        // code here
+        sort(bt.begin(), bt.end());
+        
+        long long n = bt.size();
+        
+        long long total_time = 0;
+        long long curr_time = 0;
+        
+        for(auto time: bt){
+            total_time+=curr_time;
+            curr_time+=time;
+        }
+        
+        return total_time/n;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> bt(n), p(n);
+        for (int i = 0; i < n; i++) {
+            cin >> bt[i];
+        }
+        Solution obj;
+        long long ans = obj.solve(bt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
