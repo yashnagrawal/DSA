c610f740577020506aa39ca8debaa3b6e6c09119
yashnagrawal <80106294+yashnagrawal@users.noreply.github.com>
2023-07-25 01:54:17 +0530

    Added solution - LeetHub

--- Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.cpp
+++ Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.cpp
@@ -15,33 +15,28 @@ class Solution
     long long divide(long long dividend, long long divisor) 
     {
         //code here
-        
-        int neg = ((dividend>0)^(divisor>0))?(-1):(1);
-        
-        dividend=abs(dividend);
-        divisor=abs(divisor);
-        
         long long quotient = 0;
         
-        long long mul = divisor<<31;
-        long long t = 1;
-        long long pow_of_2 = t<<31;
+        bool neg = 0;
         
-        for(int i=31; i>=0&&dividend; i--){
-            if(mul<=dividend){
-                dividend-=mul;
-                
-                quotient = quotient|pow_of_2;
+        if((dividend<0&&divisor>0)||(dividend>0&&divisor<0)) neg = 1;
+        
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+        
+        for(int i=31; i>=0; i--){
+            long long int num = divisor<<i;
+            
+            if(num<=dividend){
+                dividend-=num;
+                quotient = (quotient|(1<<i));
+                // cout<<(1<<i)<<": "<<quotient<<"\n";
             }
-            
-            // cout<<i<<": "<<quotient<<", "<<mul<<", "<<pow_of_2<<"\n";
-            
-            mul = mul>>1;
-            pow_of_2 = pow_of_2>>1;
         }
         
-        return neg*quotient;
+        if(neg) quotient = -quotient;
         
+        return quotient;
     }
 };
 
